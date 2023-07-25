f34c90e2035981d2d125dc260613f6144c6bf294
milord <miloaldevaran@yahoo.com.mx>
2023-07-25 20:52:46 +0000

    project IDs updated for buckets location

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "data-proc-tst-tfstate"
     prefix = "env/prod"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="data-proc-tst"
